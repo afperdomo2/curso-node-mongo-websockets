4530f1ee94f62c75b729e7a2fddd38f954d13ff1
alvarado0925 <alvarado0925@gmail.com>
2023-01-23 17:23:15 -0500

    🧪Feat: Use Router Express

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const router = express.Router();
+
 const app = express();
 const port = 3000;
 
-app.use('/', (req, res) => {
-    res.send('Hola');
+app.use(router);
+
+router.get('/', (req, res) => {
+    res.send('Inicio');
+});
+router.get('/message', (req, res) => {
+    res.send('Lista de mensajes');
+});
+router.post('/message', (req, res) => {
+    res.send('Mensaje añadido');
+});
+router.delete('/message', (req, res) => {
+    res.send('Mensaje borrado');
 });
 
 app.listen(port, () => {
