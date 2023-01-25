9e2710d6acbc4473a9cef61f52b43bced4233433
alvarado0925 <alvarado0925@gmail.com>
2023-01-25 17:03:36 -0500

    🧪Feat: Read and create headers

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 
 const app = express();
+// BodyParser: Permite definir los tipos de Body
+// que puede recibir el servidor, como por ejemplo:
+// json o urlencoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);// Router debe ir después de bodyParser
@@ -13,14 +16,18 @@ router.get('/', (req, res) => {
     res.send('Inicio');
 });
 router.get('/message', (req, res) => {
-    res.send('Lista de mensajes');
+    console.log(req.headers);// Imprime los headers de la solicitud
+    res.header({
+        "custom-header": "Valor del header personalizado"
+    });
+    res.send('Lista de mensajes');// Mensaje de respuesta
 });
 router.post('/message', (req, res) => {
     res.send('Mensaje añadido');
 });
 router.delete('/message', (req, res) => {
-    console.log(req.query);
-    console.log(req.body);
+    console.log(req.query);// Imprime el query de la solicitud
+    console.log(req.body);// Imprimie el body de la solicitud
     res.send(`Mensaje: ${req.body.message}`);
 });
 
