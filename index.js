9466f17bbcf3c87ada0fb64f3f328af1e421d8f2
alvarado0925 <alvarado0925@gmail.com>
2023-02-07 17:59:32 -0500

    🧪Feat: Add support and implement WebSockets

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const PORT = 8080;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado');
+    socket.emit('mensaje', 'Bienvenido!');
+});
+
+setInterval(() => {
+    io.emit('mensaje', 'hola a todos!');
+}, 3000);
+
+server.listen(8080, () => {
+    console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
